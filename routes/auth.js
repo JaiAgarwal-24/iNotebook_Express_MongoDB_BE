e67ed999304c4aad09252b367b7982f51c08a93a
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-10-16 14:40:42 +0530

    Adding Validation using Express-Validator pkg

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 // Create a User using: POST "/auth/". Dosen't require Auth
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password must be atlest 5 characters').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error: 'Please enter unique value for Email', message: err.message})})
 })
 module.exports = router;

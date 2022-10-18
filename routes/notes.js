8b88679f7182d3bb81324ec4f5ba946be5e18245
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-10-18 14:20:58 +0530

    creating end point for updating an existing note

--- routes/notes.js
+++ routes/notes.js
@@ -44,4 +44,40 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+// ROUTE 3: Update an existing Note using: PUT "/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body;
+
+        // If there are errors return basd Request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // Create a newNote object
+        const newNote = {};
+
+        if(title) {newNote.title = title};
+        if(description) {newNote.descripton = description};
+        if(tag) {newNote.tag = tag};
+
+        // Find the note to be updated and update it
+        
+         let note = await Notes.findById(req.params.id);
+         if(!note) {return res.status(404).send("Not Found")};
+         if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed")
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true} )
+
+        res.json(note);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Internal server Error")
+    }
+})
+
 module.exports = router;
